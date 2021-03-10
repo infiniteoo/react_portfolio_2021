bc1b6e417526466cc253bf315f6aef75e3ea76e6
Troy Dorman <troydorman@gmail.com>
2021-03-10 16:50:53 -0700

    still building the site.  working on skills currently.  integrating code from my previous portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar.jsx'
+import styles from "./App.module.css";
 
 class App extends React.Component {
 
     render(){
         return (
 
-            <div>
+            <div className={styles.container}>
                <NavBar/>
             </div>
 
