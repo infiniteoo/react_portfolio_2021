cdaf7354cbee2ae508080865faf0e4c7c53d9b00
Troy Dorman <troydorman@gmail.com>
2021-03-11 14:34:55 -0700

    finished up the footer component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
-import Portfolio from "./components/Pofrfolio";
+import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
       <Portfolio />
       <Testimonials />
       <Contacts />
+      <Footer />
     </>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -106,7 +106,7 @@ const Pofrfolio = () => {
 
 
   return (
-    <div className="portfolio-wrapper">
+    <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">portfolio</h1>
         <div className="image-box-wrapper row justify-content-center">
