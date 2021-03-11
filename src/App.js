c6bd8ec5c804e79da6118be1fcac8daf354bdc35
Troy Dorman <troydorman@gmail.com>
2021-03-11 14:31:52 -0700

    started working on the portfolio section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
+import Portfolio from "./components/Pofrfolio";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
       <AboutMe />
       <Services />
       <Experience />
+      <Portfolio />
     </>
   );
 }
