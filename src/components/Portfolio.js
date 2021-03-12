1c63d0d1cfa39cf3cce9c277503395808b711e1d
Troy Dorman <troydorman@gmail.com>
2021-03-12 14:33:11 -0700

    finished the portfolio examples.  on to the client testimonials

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -32,7 +32,7 @@ const Pofrfolio = () => {
   const popupboxConfigNetflix = {
     titleBar: {
       enable: true,
-      text: "Netflix clone project."
+      text: "Troy Dorman Portfolio Examples"
     },
     fadeIn: true,
     fadeInSpeed: 500
@@ -55,7 +55,7 @@ const Pofrfolio = () => {
   const popupboxConfigCityGuide = {
     titleBar: {
       enable: true,
-      text: "City Guide App project."
+      text: "Troy Dorman Portfolio Examples"
     },
     fadeIn: true,
     fadeInSpeed: 500
@@ -78,13 +78,13 @@ const Pofrfolio = () => {
   const popupboxConfigPortfolio = {
     titleBar: {
       enable: true,
-      text: "Poftfolio React and Material UI project."
+      text: "Troy Dorman Portfolio Examples"
     },
     fadeIn: true,
     fadeInSpeed: 500
   }
 
-  // Task Manager React and Redux Project
+  // Troy Dorman
   const openPopupboxTaskManager = () => {
     const content = (
       <>
@@ -101,7 +101,7 @@ const Pofrfolio = () => {
   const popupboxConfigTaskManager = {
     titleBar: {
       enable: true,
-      text: "Task Manager React and Redux project."
+      text: "Troy Dorman Portfolio Examples"
     },
     fadeIn: true,
     fadeInSpeed: 500
@@ -114,25 +114,25 @@ const Pofrfolio = () => {
         <h1 className="text-uppercase text-center py-5">portfolio</h1>
         <div className="image-box-wrapper row justify-content-center">
           <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
-            <img className="portfolio-image" src={netflix} alt="Netflix Clone Project..." />
+            <img className="portfolio-image" src={netflix} alt="Troy Dorman Portfolio Examples" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box" onClick={openPopupboxCityGuide}>
-            <img className="portfolio-image" src={cityGuide} alt="City Guide Project..." />
+            <img className="portfolio-image" src={cityGuide} alt="Troy Dorman Portfolio Examples" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box" onClick={openPopupboxPortfolio}>
-            <img className="portfolio-image" src={portfolio} alt="Portfolio React and Material UI Project..." />
+            <img className="portfolio-image" src={portfolio} alt="Troy Dorman Portfolio Examples" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
           <div className="portfolio-image-box" onClick={openPopupboxTaskManager}>
-            <img className="portfolio-image" src={taskManager} alt="Task Manager React and Redux Project..." />
+            <img className="portfolio-image" src={taskManager} alt="Troy Dorman Portfolio Examples" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
